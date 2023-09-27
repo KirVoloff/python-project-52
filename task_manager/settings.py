6f24335936f3be84122249df716b6dc74263f4f4
Vol'nov Kirill <volnov-93@mail.ru>
2023-09-27 13:29:30 +0300

    otkat

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -30,12 +30,15 @@ SECRET_KEY = 'fis5ntd-qr+^mwacrgc#!eq0zzg))=1*30uafnfa4u!f92wd42'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
+ALLOWED_HOSTS = []
 
-HOSTS = os.getenv('HOST', None)
-if HOSTS:
-    ALLOWED_HOSTS = os.getenv('HOST').split(",")
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 else:
-    ALLOWED_HOSTS = ['localhost', 'webserver']
+    ALLOWED_HOSTS.append('webserver')
+    ALLOWED_HOSTS.append('127.0.0.1')
+
 
 # Application definition
 
